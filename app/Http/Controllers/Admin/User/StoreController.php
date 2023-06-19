1a9cbfda26348e39efa18ac4a0f1514885c7a5e2
makshorror <makshorror96@gmail.com>
2023-06-19 20:39:03 +0300

    feat: 'add jobs in admin create user'

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -4,23 +4,14 @@ namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\StoreRequest;
-use App\Mail\User\PasswordMail;
-use App\Models\User;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Str;
+use App\Jobs\StoreUserJob;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
-        $password = Str::random(10);
-        $data['password'] = Hash::make($password);
-        $user = User::firstOrCreate(['email' => $data['email']], $data);
-        Mail::to($data['email'])->send(new PasswordMail($password));
-        event(new Registered($user));
+        StoreUserJob::dispatch($data);
         return redirect()->route('admin.user.index');
     }
 }
